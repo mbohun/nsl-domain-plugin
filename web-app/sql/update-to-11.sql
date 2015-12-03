74756257acdb5b25fcc718b8c581f3d066d76d0a
Paul Murray <pmurray@anbg.gov.au>
2015-12-03 14:16:46 +1100

    NSL-1558 - Add namespace to tree Untangling the mapper identifiers.

--- web-app/sql/update-to-11.sql
+++ web-app/sql/update-to-11.sql
@@ -21,3 +21,17 @@ update tree_arrangement set namespace_id = (select id from namespace where name 
 UPDATE db_version
 SET version = 11
 WHERE id = 1;
+
+-- zap bad identifiers
+
+delete from mapper.identifier_identities ii
+where exists (
+    select 1 FROM mapper.identifier i where
+      i.id = ii.identifier_id
+      and i.name_space in ('APC', 'tree')
+);
+
+delete from mapper.identifier where name_space in ('APC', 'tree');
+
+delete from mapper.match
+where uri like '%/APC/%' or uri like '%/tree/%';
