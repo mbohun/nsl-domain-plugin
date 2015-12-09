f37cc77f3577e15e5bbb65e74293c71465026ea4
Greg Clarke <gregbclarke@gmail.com>
2015-12-09 13:08:35 +1100

    NSL-1560: Add text search index for reference citation.

--- web-app/sql/views/other-setup.sql
+++ web-app/sql/views/other-setup.sql
@@ -2,4 +2,6 @@ ALTER TABLE instance ADD CONSTRAINT citescheck CHECK (cites_id IS NULL OR cited_
 
 CREATE INDEX name_lower_f_unaccent_full_name_like ON name (lower(f_unaccent(full_name)) varchar_pattern_ops);
 
+CREATE INDEX ref_citation_text_index ON reference USING gin(to_tsvector('english'::regconfig,f_unaccent(coalesce((citation)::text,''::text))));
+
 INSERT INTO db_version (id, version) VALUES (1, 11);
