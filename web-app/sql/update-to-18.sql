fe4ec726f26315b1048f92c7971fbddb405e734a
Paul Murray <pmurray@anbg.gov.au>
2016-08-17 17:08:44 +1000

    speed up various tree queries

--- web-app/sql/update-to-18.sql
+++ web-app/sql/update-to-18.sql
@@ -19,6 +19,18 @@ Update namespace set rdf_id = 'algae' where name = 'Algae';
 INSERT INTO namespace (id, lock_version, name, description_html, rdf_id) VALUES (nextval('nsl_global_seq'), 0, 'Lichen', '(description of <b>Lichen</b>)', 'lichen');
 INSERT INTO public.namespace (id, lock_version, name, description_html, rdf_id) VALUES (nextval('nsl_global_seq'), 0, 'Fungi', '(description of <b>Fungi</b>)', 'fungi');
 
+
+create index idx_node_current_name_a on tree_node(name_id, tree_arrangement_id) where replaced_at_id is null;
+
+ create index idx_node_current_name_b on tree_node(name_id, tree_arrangement_id) where next_node_id is null; 
+
+create index idx_node_current_instance_a on tree_node(instance_id, tree_arrangement_id) where replaced_at_id is null; 
+
+create index idx_node_current_instance_b on tree_node(instance_id, tree_arrangement_id) where next_node_id is null; 
+
+
+
+
 -- version
 UPDATE db_version
 SET version = 18
