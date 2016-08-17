a12a9327f19d65007b6ff9ce3fa8db234f9e237c
pmcneil <peter@nerderg.com>
2016-08-17 10:35:29 +1000

    Add no duplicate synonyms constraint. see https://anbg-ibis.slack.com/archives/general/p1471333080000079

--- web-app/sql/update-to-18.sql
+++ web-app/sql/update-to-18.sql
@@ -1,5 +1,8 @@
+ALTER TABLE instance
+  ADD CONSTRAINT no_duplicate_synonyms UNIQUE (name_id, reference_id, instance_type_id, page, cites_id, cited_by_id);
+
 ALTER TABLE tree_arrangement
-  ADD COLUMN shared boolean DEFAULT true;
+  ADD COLUMN shared BOOLEAN DEFAULT TRUE;
 
 -- version
 UPDATE db_version
