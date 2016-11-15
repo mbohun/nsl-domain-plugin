a48e60d9310b48ffe24f43a0ced5fee6abc69212
Paul Murray <pmurray@anbg.gov.au>
2016-11-15 13:53:13 +1100

    Pushing new stored procedure into database update.

--- grails-app/services/au/org/biodiversity/nsl/NslDomainService.groovy
+++ grails-app/services/au/org/biodiversity/nsl/NslDomainService.groovy
@@ -21,7 +21,7 @@ class NslDomainService {
     def pluginManager
     def grailsApplication
 
-    static final Integer currentVersion = 19
+    static final Integer currentVersion = 20
 
     File getDdlFile() {
         GrailsPlugin plugin = pluginManager.allPlugins.find { it.name.startsWith('nslDomain') }
