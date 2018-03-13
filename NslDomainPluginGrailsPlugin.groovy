fdaec8de98ccac9f9b2395f3599d2ba79f085057
pmcneil <peter@nerderg.com>
2018-03-14 10:00:24 +1100

    Release domain plugin updates for 1.7

--- NslDomainPluginGrailsPlugin.groovy
+++ NslDomainPluginGrailsPlugin.groovy
@@ -15,7 +15,7 @@
 */
 class NslDomainPluginGrailsPlugin {
     // the plugin version
-    def version = "1.7-SNAPSHOT"
+    def version = "1.7"
     def groupId = 'au.org.biodiversity.grails.plugins'
 
     // the version or versions of Grails the plugin is designed for
