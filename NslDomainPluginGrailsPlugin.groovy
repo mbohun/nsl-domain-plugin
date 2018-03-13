ff0ae00a84430d7f6690d9ecf4dee1670757dcde
pmcneil <peter@nerderg.com>
2018-03-14 10:06:16 +1100

    Bump to version 1.8-SNAPSHOT on master

--- NslDomainPluginGrailsPlugin.groovy
+++ NslDomainPluginGrailsPlugin.groovy
@@ -15,7 +15,7 @@
 */
 class NslDomainPluginGrailsPlugin {
     // the plugin version
-    def version = "1.7"
+    def version = "1.8-SNAPSHOT"
     def groupId = 'au.org.biodiversity.grails.plugins'
 
     // the version or versions of Grails the plugin is designed for
