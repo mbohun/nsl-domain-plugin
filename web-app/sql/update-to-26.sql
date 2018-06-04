90a601b522ea074cb028a7f1f347a50e49a50526
pmcneil <peter@nerderg.com>
2018-06-04 12:44:40 +1000

    Bump version to 1.10-SNAPSHOT set DB version to 26 Add default update to 26 script

---
+++ web-app/sql/update-to-26.sql
@@ -0,0 +1,5 @@
+-- version
+UPDATE db_version
+SET version = 26
+WHERE id = 1;
+
--- web-app/sql/views/other-setup.sql
+++ web-app/sql/views/other-setup.sql
@@ -39,4 +39,4 @@ CREATE INDEX tree_synonyms_index
 ALTER TABLE tree
   ADD CONSTRAINT draft_not_current CHECK (current_tree_version_id <> default_draft_tree_version_id);
 --
-INSERT INTO db_version (id, version) VALUES (1, 25);
+INSERT INTO db_version (id, version) VALUES (1, 26);
