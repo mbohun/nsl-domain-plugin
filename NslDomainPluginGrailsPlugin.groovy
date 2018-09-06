15ebe505d321efd65ad38e4ce969dffcc2ef38ef
pmcneil <peter@nerderg.com>
2018-09-06 14:07:21 +1000

    Set release version to 1.11

--- NslDomainPluginGrailsPlugin.groovy
+++ NslDomainPluginGrailsPlugin.groovy
@@ -15,7 +15,7 @@
 */
 class NslDomainPluginGrailsPlugin {
     // the plugin version
-    def version = "1.11-SNAPSHOT"
+    def version = "1.11"
     def groupId = 'au.org.biodiversity.grails.plugins'
 
     // the version or versions of Grails the plugin is designed for
